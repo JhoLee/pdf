23d48b6426b05d495952b5db9131031a01fdb9e6
JhoLee <jho.lee@kakao.com>
2020-05-13 17:47:10 +0900

    Init blur app'

--- pdf_front/pdf_front/settings.py
+++ pdf_front/pdf_front/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
+    'blur.apps.BlurConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
